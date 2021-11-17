fc64b7ac79e9988ac839a29f30d7698ced9c66cc
afsanana <80018765+afsanana@users.noreply.github.com>
2021-11-16 17:02:35 -0700

    Patient input/output Afsana Salahudeen

--- src/sample/PatientInfoController2.java
+++ src/sample/PatientInfoController2.java
@@ -8,6 +8,8 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.*;
@@ -16,12 +18,68 @@ import java.util.*;
 
 public class PatientInfoController2 {
     @FXML
-    Label patientName;
-
-
+    Label welcome; 
+    @FXML
+    TextField savedFirst;
+    @FXML
+    TextField savedLast;
+    @FXML
+    TextField savedBirth;
+    @FXML
+    TextField savedAddr;
+    @FXML
+    TextField savedNum;
+    @FXML
+    TextField savedEmail;
+    @FXML
+    TextField savedPCN;
+    @FXML
+    TextField savedRelation;
+    @FXML
+    TextField savedCN;
+    @FXML
+    TextField savedInsurance;
+    @FXML
+    TextField savedMemId;
+    @FXML
+    TextField savedGN;
+    @FXML
+    TextField subject;
+    @FXML
+    TextArea message;
+    
+    private int updatePatientNum = 1;
+    
     private Stage stage;
     private Scene scene;
     private Parent root;
+    
+    public void welcomeName(String fullName)
+    {
+    	welcome.setText("Welcome " + fullName);
+    }
+    
+
+    public void setUpInputs(String f, String l, String b, String addr, String pn, String e, String pcn, String r, String cn, String i, String m, String gn)
+    {
+    	savedFirst.setText(f);
+    	savedLast.setText(l);
+    	savedBirth.setText(b);
+    	savedAddr.setText(addr);
+    	savedNum.setText(pn);
+    	savedEmail.setText(e);
+    	savedPCN.setText(pcn);
+    	savedRelation.setText(r);
+	  	savedCN.setText(cn);
+	  	savedInsurance.setText(i);
+	  	savedMemId.setText(m);
+	  	savedGN.setText(gn);
+    }
+    
+    public void patientNum(int num)
+    {
+    	updatePatientNum = num;
+    }
     
     public void logOut(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
@@ -31,10 +89,187 @@ public class PatientInfoController2 {
         stage.show();
     }
     
-    public void saveChange(ActionEvent event) throws IOException {
+    public void saveChange(ActionEvent event) throws IOException 
+    {
+    	updatePatientNum = 1;
+    	//to find patient in text file
+    	if (searchPatient(savedFirst.getText().toString(), savedLast.getText().toString(), savedBirth.getText().toString()))
+    	{
+    		int index =  updatePatientNum;  
+    		Scanner fileReader = new Scanner(new File("PatientData.txt")); 
+        	for (int i = 0; i < updatePatientNum-1; i++)
+        	{
+        		if(fileReader.hasNextLine())
+        		{
+        			fileReader.nextLine();
+        		}
+        	}
+        	fileReader.close();
+    	}
+   	
+        int index = updatePatientNum;	
+    	String newInfo = savedFirst.getText().toString() + "\t" +
+    						savedLast.getText().toString() + "\t" +
+    						savedBirth.getText().toString() + "\t" +
+    						savedAddr.getText().toString() + "\t" +
+    						savedNum.getText().toString() + "\t" +
+    						savedEmail.getText().toString() + "\t" +
+    						savedPCN.getText().toString() + "\t" +
+    						savedRelation.getText().toString() + "\t" +
+    						savedCN.getText().toString() + "\t" +
+    						savedInsurance.getText().toString() + "\t" +
+    						savedMemId.getText().toString() + "\t" +
+    						savedGN.getText().toString() + "\t";
+    	
+    	//remove patient info
+    	delete(index+1);
+
+    	//add in new info to text file
+    	try(BufferedWriter bf = new BufferedWriter(new FileWriter("PatientData.txt", true)))
+    	{
+    		bf.write(newInfo);
+    	}
+    	catch (IOException ex)
+    	{
+    		
+    	}
+    	
+    
+    	
+    	
     }
     
+    //to delete a patient 
+    void delete(int startline)
+	{
+		try
+		{
+			BufferedReader br=new BufferedReader(new FileReader("PatientData.txt"));
+ 
+			//String buffer to store contents of the file
+			StringBuffer sb=new StringBuffer("");
+ 
+			//Keep track of the line number
+			int linenumber=1;
+			String line;
+ 
+			while((line=br.readLine())!=null)
+			{
+				//Store each valid line in the string buffer
+				if(linenumber<startline||linenumber>=startline+1)
+					sb.append(line+"\n");
+				linenumber++;
+			}
+			br.close();
+ 
+			FileWriter fw=new FileWriter(new File("PatientData.txt"));
+			
+			//Write entire string buffer into the file
+			fw.write(sb.toString());
+			fw.close();
+		}
+		catch (Exception e)
+		{
+			System.out.println("something went wrong");
+		}
+	}
+    
     public void sendMessage(ActionEvent event) throws IOException {
+    	
+    	updatePatientNum = 1;
+    	//to find patient in text file
+    	if (searchPatient(savedFirst.getText().toString(), savedLast.getText().toString(), savedBirth.getText().toString()))
+    	{
+    		int index =  updatePatientNum;  
+    		Scanner fileReader = new Scanner(new File("PatientData.txt")); 
+        	for (int i = 0; i < updatePatientNum-1; i++)
+        	{
+        		if(fileReader.hasNextLine())
+        		{
+        			fileReader.nextLine();
+        		}
+        	}
+        	fileReader.nextLine();
+       	
+        	String newInfo = fileReader.nextLine() + 
+        						"Subject: " + subject.getText().toString() + "\tMessage: " + message.getText().toString() + "\t";
+        	
+        	//remove patient info
+//        	if(index == 1)
+//        	{
+//        		index++;
+//        	}
+        	delete(index+1);
+
+        	//add in new info to text file
+        	try(BufferedWriter bf = new BufferedWriter(new FileWriter("PatientData.txt", true)))
+        	{
+        		bf.write(newInfo);
+        	}
+        	catch (IOException ex)
+        	{
+        		
+        	}
+        	
+        	fileReader.close();
+    	}
+    	
+    	
+
+    	//reset text boxes
+    	subject.setText("");
+    	message.setText("");
     }
+    
+    
+    public boolean searchPatient(String First, String Last, String Birthday) throws IOException{
+    	boolean flag = false;
+    	
+    	Scanner fileReader = new Scanner(new File("PatientData.txt")); 
+    	
+    	while(fileReader.hasNextLine())
+    	{
+    		//grab first name string
+    		String compF = fileReader.next("[\\S ]+");
+    		if(compF.equals(First))
+    		{
+    			//grab last name string
+    			String compL = fileReader.next("[\\S ]+");
+    			if (compL.equals(Last)) 
+    			{
+    				// grab birthday string
+    				String compB = fileReader.next("[\\S ]+");
+        			if (compB.equals(Birthday.toString()))
+        			{
+        				// if everything equals, flag is true
+        				flag = true;
+            			break;
+        			}
+        			else //go to next patient
+        			{
+        				fileReader.nextLine();
+        				updatePatientNum++;
+        			} 				
+    			}
+    			else //go to next patient
+    			{
+    				fileReader.nextLine();
+    				updatePatientNum++;
+    			}
+    		}
+    		else //go to next patient
+    		{
+    			fileReader.nextLine();
+    			updatePatientNum++;
+    		}
+    		//System.out.println("flag is " + flag);
+    	}
+    	
+    	fileReader.close();
+    	
+    	
+    	return flag;
+    }
+
 
 }
